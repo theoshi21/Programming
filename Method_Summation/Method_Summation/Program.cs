8db158fd99c942ff7fea1d4ab914ab412875c1aa
theoshi21 <kelvinignacio2110@gmail.com>
2023-11-12 11:44:17 +0800

    First Commit

---
+++ Method_Summation/Method_Summation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add();
+        }
+
+        static int summation(int[] x)
+        {
+            int sum = 0;
+            foreach (int i in x)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        static void add()
+        {
+            Console.Write("Number to input: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] num = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Number {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The sum is {summation(num)}");
+        }
+    }
+}
