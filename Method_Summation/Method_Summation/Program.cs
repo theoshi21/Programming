35c5e6758f714ba0bec9fec74ee85f5a96b1d67d
theoshi21 <kelvinignacio2110@gmail.com>
2023-11-12 12:02:03 +0800

    updated program.cs

--- Method_Summation/Method_Summation/Program.cs
+++ Method_Summation/Method_Summation/Program.cs
@@ -10,9 +10,21 @@ namespace Method_Summation
     {
         static void Main(string[] args)
         {
-            add();
+            start();
         }
 
+        static void start()
+        {
+            Console.Write("What do you want to perform?\n[1] Add\n[2] Multply\nAnswer: ");
+            int operation = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case 1: add(); break;
+                case 2: multiply(); break;
+                default: Console.WriteLine("Invalid operation."); break;
+            }
+        }
         static int summation(int[] x)
         {
             int sum = 0;
@@ -24,8 +36,20 @@ namespace Method_Summation
             return sum;
         }
 
+        static int multiplication(int[] x)
+        {
+            int prod = 1;
+            foreach (int i in x)
+            {
+                prod *= i;
+            }
+
+            return prod;
+        }
+
         static void add()
         {
+            Console.WriteLine("---------\nADDITION\n---------");
             Console.Write("Number to input: ");
             int length = int.Parse(Console.ReadLine());
             int[] num = new int[length];
@@ -37,6 +61,22 @@ namespace Method_Summation
             }
 
             Console.WriteLine($"The sum is {summation(num)}");
+        }
+
+        static void multiply()
+        {
+            Console.WriteLine("---------\nMULTIPLICATION\n---------");
+            Console.Write("Number to input: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] num = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Number {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The product is {multiplication(num)}");
         }
     }
 }
